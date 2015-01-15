affd8599bfd57c000f6ac6f6c1bffe726fb894db
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-01-15 00:32:05 +0000

    Updated

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,13 +1,13 @@
 module.exports = {
-	all: {
-		files: [
-			{
-				expand: true,
-				src: ['src/copy/*'],
-				dest: 'dist/',
-				flatten: true,
-				filter: 'isFile'
-			}
-		]
-	}
+  all: {
+    files: [
+      {
+        expand: true,
+        src: ['src/copy/*'],
+        dest: 'dist/',
+        flatten: true,
+        filter: 'isFile'
+      }
+    ]
+  }
 };
--- grunt/imagemin.js
+++ grunt/imagemin.js
@@ -1,10 +1,10 @@
 module.exports = {
-    all: {
-        files: [{
-            expand: true,
-            cwd: 'src/',
-            src: ['images/*.{png,jpg,gif}'],
-            dest: 'dist/'
-        }]
-    }
+  all: {
+    files: [{
+      expand: true,
+      cwd: 'src/',
+      src: ['images/*.{png,jpg,gif}'],
+      dest: 'dist/'
+    }]
+  }
 };
